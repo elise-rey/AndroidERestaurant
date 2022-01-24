6ec39ea9702d5440965681490c0ca10e025ab222
elise.rey <elise.rey@isen.yncrea.fr>
2022-01-24 14:53:54 +0100

    Toasts on each button

--- app/src/main/java/fr/isen/rey/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/rey/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,27 @@ package fr.isen.rey.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import fr.isen.rey.androiderestaurant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listenClick()
+    }
+    private fun listenClick() {
+        binding.starters.setOnClickListener() {
+            Toast.makeText(applicationContext, "Ouverture des entrées...", Toast.LENGTH_SHORT).show()
+        }
+        binding.courses.setOnClickListener() {
+            Toast.makeText(applicationContext, "Ouverture des plats...", Toast.LENGTH_SHORT).show()
+        }
+        binding.desserts.setOnClickListener() {
+            Toast.makeText(applicationContext, "Ouverture des desserts...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
